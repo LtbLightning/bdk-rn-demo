bd4adb4abc91f179fc7b1a1af19ba3cb0193239e
BitcoinZavior <BitcoinZavior@gmail.com>
2022-05-10 21:22:55 -0400

    wallet exists, restore + async storage

--- App.tsx
+++ App.tsx
@@ -1,52 +1,88 @@
-import React, {useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {Button, Text, View} from 'react-native';
 import RnBdk from 'bdk-rn';
 
 const App = () => {
   const [response, _response] = useState();
+  const [walletExists, _walletExists] = useState(false);
+  const [walletUnlocked, _walletUnlocked] = useState(false);
+  const [mnemonic, _mnemonic] = useState(
+    'title screen science betray fiber brother differ sniff page put damage slender',
+  );
+
+  useEffect(() => {
+    (async () => {
+      const exists = await RnBdk.walletExists();
+      _walletExists(exists.data);
+    })();
+  }, []);
+
+  const setResponse = (res: any) => _response(JSON.stringify(res.data));
+
   const getNewAddress = async () => {
     const res = await RnBdk.getNewAddress();
-    _response(res.data);
+    setResponse(res);
   };
 
   const getBalance = async () => {
     const res = await RnBdk.getBalance();
-    _response(res.data);
+    setResponse(res);
   };
 
   const broadcastTx = async () => {
-    const res = await RnBdk.broadcastTx(
-      'tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt',
-      700,
-    );
-    _response(JSON.stringify(res.data));
+    const res = await RnBdk.broadcastTx('tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt', 600);
+    setResponse(res);
   };
 
   const createWallet = async () => {
     const res = await RnBdk.createWallet();
-    _response(JSON.stringify(res.data));
+    setResponse(res);
+
+    _walletUnlocked(!res.error);
+    _walletExists(!res.error);
   };
 
   const restoreWallet = async () => {
-    const res = await RnBdk.restoreWallet(
-      'box limit letter buddy endorse crush pulp copy immune dynamic phrase initial',
-    );
-    _response(JSON.stringify(res.data));
+    const res = await RnBdk.restoreWallet(mnemonic);
+    setResponse(res);
+    _walletUnlocked(!res.error);
+    _walletExists(!res.error);
   };
 
   const genSeed = async () => {
     const res = await RnBdk.genSeed();
-    _response(JSON.stringify(res.data));
+    setResponse(res);
+  };
+
+  const unlockWallet = async () => {
+    const res = await RnBdk.unlockWallet();
+    setResponse(res);
+    _walletUnlocked(!res.error);
   };
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Button title="Gen Seed" onPress={() => genSeed()} />
-      <Button title="Create new wallet" onPress={() => createWallet()} />
-      <Button title="Restore wallet" onPress={() => restoreWallet()} />
-      <Button title="Get new address" onPress={() => getNewAddress()} />
-      <Button title="Get balance" onPress={() => getBalance()} />
-      <Button title="Broadcast TX" onPress={() => broadcastTx()} />
+      {!walletExists && !walletUnlocked && (
+        <Fragment>
+          <Button title="Gen Seed" onPress={() => genSeed()} />
+          <Button title="Create new wallet" onPress={() => createWallet()} />
+          <Button title="Restore wallet" onPress={() => restoreWallet()} />
+        </Fragment>
+      )}
+
+      {walletExists && !walletUnlocked && (
+        <Fragment>
+          <Button title="Unlock Wallet!" onPress={() => unlockWallet()} />
+        </Fragment>
+      )}
+
+      {walletExists && walletUnlocked && (
+        <Fragment>
+          <Button title="Get new address" onPress={() => getNewAddress()} />
+          <Button title="Sync Wallet & Get balance" onPress={() => getBalance()} />
+          <Button title="Broadcast TX" onPress={() => broadcastTx()} />
+        </Fragment>
+      )}
       <Text selectable>{response}</Text>
     </View>
   );
--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,17 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
+  },
+  resolver: {
+    blockList: exclusionList([
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
+    ]),
   },
 };
