4a30a4173b7a1c3ba341862234845d76f442e690
BitcoinZavior <BitcoinZavior@gmail.com>
2022-05-10 21:15:13 -0400

    create and restore wallet

--- App.tsx
+++ App.tsx
@@ -13,14 +13,35 @@ const App = () => {
     const res = await RnBdk.getBalance();
     _response(res.data);
   };
+
+  const broadcastTx = async () => {
+    const res = await RnBdk.broadcastTx(
+      'tb1ql7w62elx9ucw4pj5lgw4l028hmuw80sndtntxt',
+      700,
+    );
+    _response(JSON.stringify(res.data));
+  };
+
+  const createWallet = async () => {
+    const res = await RnBdk.createWallet();
+    _response(JSON.stringify(res.data));
+  };
+
+  const restoreWallet = async () => {
+    const res = await RnBdk.restoreWallet();
+    _response(JSON.stringify(res.data));
+  };
+
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Button title="Create new wallet" onPress={() => createWallet()} />
+      <Button title="Restore wallet" onPress={() => restoreWallet()} />
       <Button title="Get new address" onPress={() => getNewAddress()} />
       <Button title="Get balance" onPress={() => getBalance()} />
+      <Button title="Broadcast TX" onPress={() => broadcastTx()} />
       <Text selectable>{response}</Text>
     </View>
   );
 };
 
 export default App;
-
