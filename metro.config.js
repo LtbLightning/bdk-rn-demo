fdebc10416aa35a15492aeffb75f77eb8aa4020e
BitcoinZavior <BitcoinZavior@GMail.Com>
2024-10-16 08:22:00 -0400

    Upgraded to latest RN

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,11 @@
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    blockList: exclusionList([
-      /node_modules\/.*\/node_modules\/react-native\/.*/,
-    ]),
-  },
-};
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
